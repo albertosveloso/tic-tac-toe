78381f8d8ac72a5f754126939c208f480652eb28
Alberto Veloso <alberto.veloso@linx.com.br>
2019-08-12 15:36:40 -0300

    Updates

---
+++ precache-manifest.b7dec449bdc77faf9b6f8fbebbd7dfd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d6dcb77ece328406b127846d7a5f63",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "e47a7e21cccab9ddd782",
+    "url": "/tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "bcb866a3cf9e0efa1d30",
+    "url": "/tic-tac-toe/static/js/2.6a465692.chunk.js"
+  },
+  {
+    "revision": "e47a7e21cccab9ddd782",
+    "url": "/tic-tac-toe/static/js/main.c3a3e102.chunk.js"
+  },
+  {
+    "revision": "2447e331fa6994ee754e",
+    "url": "/tic-tac-toe/static/js/runtime~main.96c826cc.js"
+  }
+]);
